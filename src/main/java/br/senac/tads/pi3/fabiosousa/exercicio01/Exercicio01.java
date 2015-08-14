81813ff76dbacc2ff0db318f1474aef78c622968
fabio.ssanto10 <fabio.ssanto10@CAS486194C14732.senacsp.edu.br>
2015-08-14 20:06:10 -0300

    inclusão do main

---
+++ src/main/java/br/senac/tads/pi3/fabiosousa/exercicio01/Exercicio01.java
@@ -0,0 +1,11 @@
+
+package br.senac.tads.pi3.fabiosousa.exercicio01;
+
+// @author fabio.ssanto10
+ 
+public class Exercicio01 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+       
+    }
+}
