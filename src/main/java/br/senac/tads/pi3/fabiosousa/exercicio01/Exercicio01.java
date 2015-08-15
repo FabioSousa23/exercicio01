21ac491bb1e1eff8413c7a4dd313e9c5f25eb364
fabio.ssanto10 <fabio.ssanto10@CAS486194C14732.senacsp.edu.br>
2015-08-14 21:44:45 -0300

    inclusão

--- src/main/java/br/senac/tads/pi3/fabiosousa/exercicio01/Exercicio01.java
+++ src/main/java/br/senac/tads/pi3/fabiosousa/exercicio01/Exercicio01.java
@@ -6,6 +6,11 @@ package br.senac.tads.pi3.fabiosousa.exercicio01;
 public class Exercicio01 {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+       int num = 0;
+        if(num>2){
+        
+        }
+                
        
     }
 }
