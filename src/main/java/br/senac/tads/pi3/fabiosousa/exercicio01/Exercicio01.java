b257aaf6888262a43e8809b9b03c20121d34d8ed
fabio.ssanto10 <fabio.ssanto10@CAS486194C14732.senacsp.edu.br>
2015-08-14 22:14:39 -0300

    inclusão

--- src/main/java/br/senac/tads/pi3/fabiosousa/exercicio01/Exercicio01.java
+++ src/main/java/br/senac/tads/pi3/fabiosousa/exercicio01/Exercicio01.java
@@ -1,16 +1,28 @@
-
 package br.senac.tads.pi3.fabiosousa.exercicio01;
 
 // @author fabio.ssanto10
- 
+import java.sql.*;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Exercicio01 {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
-       int num = 0;
-        if(num>2){
+        Scanner leitor = new Scanner(System.in);
         
-        }
-                
-       
+        System.out.println("Cadastro de Contatos");
+        System.out.println("Nome:");
+        String nome = leitor.nextLine();
+        System.out.println("Data de Nascimento:");
+        String dataNasc = leitor.nextLine();
+        System.out.println("E-mail:");
+        String email = leitor.nextLine();
+        System.out.println("Telefone:");
+        String tel = leitor.nextLine();
+        
+        
+        
+        
     }
 }
